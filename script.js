4bc53af6dae116f3cb829036389634af076fb913
NomanLodhi <110075584+NomanLodhi@users.noreply.github.com>
2024-05-22 14:35:47 -0700

    content

---
+++ script.js
@@ -0,0 +1,7 @@
+const images=document.querySelector('.img-gallery');
+const modal=document.getElementById('img-modal')
+const modalContent=document.getElementById('img-modal-content')
+images.addEventListener('click',()=>{
+    // modal.style.display='flex'
+    console.log('clicked')
+})
